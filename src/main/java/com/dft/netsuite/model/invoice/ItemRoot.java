578d770e6675f6fd1b74f1748a8bbf674e63ae2a
milan.lathiya <milan.lathiya@dakshyafreondtech.com>
2023-06-07 16:42:18 +0530

    AN-8 Updated version and set NON_NULL.

--- src/main/java/com/dft/netsuite/model/invoice/ItemRoot.java
+++ src/main/java/com/dft/netsuite/model/invoice/ItemRoot.java
@@ -1,10 +1,12 @@
 package com.dft.netsuite.model.invoice;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ItemRoot {
 
     private List<Item> items;
