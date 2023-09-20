ed1e53ec3fc2305ea8f43b93a406e9383f13b870
milan.lathiya <milan.lathiya@dakshyafreondtech.com>
2023-09-20 10:42:27 +0530

    NIJATECH-8 Added create customer payment method and related models.

---
+++ src/main/java/com/dft/netsuite/model/customerpayment/CustomerPaymentRequest.java
@@ -0,0 +1,29 @@
+package com.dft.netsuite.model.customerpayment;
+
+import com.dft.netsuite.model.invoice.IdFiled;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CustomerPaymentRequest {
+
+    @JsonProperty("customForm")
+    private IdFiled customForm;
+
+    @JsonProperty("account")
+    private IdFiled account;
+
+    @JsonProperty("customer")
+    private IdFiled customer;
+
+    @JsonProperty("apply")
+    private Apply apply;
+
+    @JsonProperty("location")
+    private IdFiled location;
+
+    @JsonProperty("memo")
+    private String memo;
+}
